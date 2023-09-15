c855f2186a00151b147ce1a41d3b5981b4142fad
tnt <73168136+tnnnnt@users.noreply.github.com>
2023-09-15 11:08:43 +0800

    利用配置文件标记实现禁止多开功能

--- main.py
+++ main.py
@@ -6,9 +6,22 @@ import public_data as pdt
 from my_system_tray_icon import MySystemTrayIcon
 import csv
 
+
+def on_exit():
+    # 在这个槽函数中执行你想要的操作
+    with open('run.txt', 'w', encoding='utf-8') as f:
+        f.write('0')  # 将程序运行标记重置为0
+
+
 if __name__ == '__main__':
-    with open('words.csv', 'r', newline='', encoding='utf-8') as file:
-        csv_reader = csv.DictReader(file)
+    # 从文件加载当前库存信息
+    with open('run.txt', 'r', encoding='utf-8') as f:
+        if f.read() == '1':
+            sys.exit()
+    with open('run.txt', 'w', encoding='utf-8') as f:
+        f.write('1')
+    with open('words.csv', 'r', newline='', encoding='utf-8') as f:
+        csv_reader = csv.DictReader(f)
         for row in csv_reader:
             english = row['英文']
             weight = int(row['权重'])  # 将权重转换为整数
@@ -26,4 +39,5 @@ if __name__ == '__main__':
     pdt.tp = MySystemTrayIcon()
     pdt.tp.show()
     pdt.tp.showMessage('单词弹弹弹', '欢迎使用单词弹弹弹！', pdt.icon)
+    app.aboutToQuit.connect(on_exit)
     sys.exit(app.exec_())
