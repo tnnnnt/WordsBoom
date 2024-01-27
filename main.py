72aa602f8457f0a10cbff853108d54e7eca5a3db
tnt <73168136+tnnnnt@users.noreply.github.com>
2024-01-27 19:48:23 +0800

    利用文件锁改进了“防止多开”的功能，修复了以往程序运行崩溃导致无法重启的情况
    参考文献：https://blog.csdn.net/qq_40602000/article/details/108655616

--- main.py
+++ main.py
@@ -1,27 +1,37 @@
+# coding:utf-8
 import json
 import sys
+import os
+import csv
+import msvcrt
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QApplication
 import public_data as pdt
 from word_editing import WordEditing
 from viewer_window import ViewerWindow
 from my_system_tray_icon import MySystemTrayIcon
-import csv
 
 
-def on_exit():
-    # 在这个槽函数中执行你想要的操作
-    with open('run.txt', 'w', encoding='utf-8') as file:
-        file.write('0')  # 将程序运行标记重置为0
+# 文件锁
+def checkSingleProcess():
+    global PIDFILE
+    try:
+        PIDFILE = open(os.path.realpath(sys.argv[0]), "r")  # 获取运行的文件路径
+        msvcrt.locking(PIDFILE.fileno(), msvcrt.LK_NBLCK, 1)  # 尝试以非阻塞方式锁定文件
+    except IOError:
+        # 如果文件已被锁定，表示程序已在运行
+        # print("相同程序正在运行")
+        return False
+    return True
 
 
 if __name__ == '__main__':
-    # 从文件加载当前库存信息
-    with open('run.txt', 'r', encoding='utf-8') as f:
-        if f.read() == '1':
-            sys.exit()
-    with open('run.txt', 'w', encoding='utf-8') as f:
-        f.write('1')
+    PIDFILE = None  # 初始化 PIDFILE
+    # 防止多开
+    if not checkSingleProcess():
+        sys.exit(-1)
+
+    # 从 CSV 文件中读取单词数据并存储到全局变量中
     with open('words.csv', 'r', newline='', encoding='utf-8') as f:
         csv_reader = csv.DictReader(f)
         for row in csv_reader:
@@ -29,22 +39,31 @@ if __name__ == '__main__':
             weight = int(row['权重'])  # 将权重转换为整数
             chinese = row['中文']
             pdt.words[english] = [weight, chinese]
+
+    # 从 JSON 文件中读取设置信息并存储到全局变量中
     with open('settings.json', encoding='UTF-8') as f:
         pdt.settings = json.load(f)
+
+    # 从 JSON 文件中读取简单单词信息并存储到全局变量中
     with open('easy_words.json', encoding='UTF-8') as f:
         pdt.easy_words = json.load(f)
+
+    # 从 JSON 文件中读取困难单词信息并存储到全局变量中
     with open('hard_words.json', encoding='utf-8') as f:
         pdt.hard_words = json.load(f)
+
+    # 初始化应用程序
     app = QApplication(sys.argv)
-    app.setQuitOnLastWindowClosed(False)
+    app.setQuitOnLastWindowClosed(False)  # 窗口关闭时不退出应用程序
     pdt.icon = QIcon('img/icon.png')
 
+    # 初始化单词编辑器和单词查看器窗口
     pdt.word_editing = WordEditing()
     pdt.vw = ViewerWindow()
 
+    # 初始化系统托盘图标
     pdt.tp = MySystemTrayIcon()
-    pdt.tp.set_set()
-    pdt.tp.showMessage('单词弹弹弹', '欢迎使用单词弹弹弹！', pdt.icon)
-    pdt.tp.show()
-    app.aboutToQuit.connect(on_exit)
-    sys.exit(app.exec_())
+    pdt.tp.set_set()  # 设置系统托盘图标
+    pdt.tp.showMessage('单词弹弹弹', '欢迎使用单词弹弹弹！', pdt.icon)  # 显示欢迎消息
+    pdt.tp.show()  # 显示系统托盘图标
+    sys.exit(app.exec_())  # 运行应用程序并进入事件循环
